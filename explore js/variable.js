cfc1288473638ebfaabeb46a57be5f50ae54b4ac
unknown <aktershanjida414@gmail.com>
2021-01-19 23:24:54 -0800

    this is another one

---
+++ explore js/variable.js
@@ -0,0 +1,13 @@
+/*var schoolName;
+schoolName = ["bepza", "BN", "Katakhali"];
+
+console.log(schoolName);*/
+var car = {
+    type: "Fiat",
+    model: "500",
+    color: "white"
+};
+console.log(car);
+var inch = 190;
+var feet = inch / 12;
+console.log(feet);
