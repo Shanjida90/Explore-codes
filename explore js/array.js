2542b9bdc44002903a3d63d53ffdab8d52acada3
unknown <aktershanjida414@gmail.com>
2021-01-19 23:22:02 -0800

    this is a js file

---
+++ explore js/array.js
@@ -0,0 +1,14 @@
+// var fruitList = ["banana, blackberry, mango, jackfruit"];
+// var favouriteOne = fruitList[2];
+
+
+// console.log(favouriteOne);
+// var friendsAge = [12, 13, 15, 25];
+// var somaAge = friendsAge[2];
+// var position = friendsAge.indexOf(25);
+// friendsAge[3] = 19; // ata hocceage reset
+// console.log(friendsAge.length);
+var colorName = ["red", "blue", "black", "green"]
+
+colorName.unshift("golden ");
+console.log(colorName);
